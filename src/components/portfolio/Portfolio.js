ced917614eff566c5561fb470704c9e7371da0f9
atramUCSD <atram@ucsd.edu>
2022-08-06 01:23:12 -0700

    First commit in new repository

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PortfolioBlock from "./PortfolioBlock";
 import {Box, Grid} from "@mui/material";
 import {info} from "../../info/Info";
+import Style from './Portfolio.module.scss';
 
 export default function Portfolio() {
     return (
-        <Box>
+        <Box className = {Style.fadeIn}>
             <Grid container display={'flex'} justifyContent={'center'}>
                 {info.portfolio.map(project => (
                    <Grid item xs={12} md={4}>
